c8ac4f2128b89f7c2253cd217779f7de0bda41de
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-08-06 17:33:51 +0900

    Implement spec to check class methods

--- lib/simple_paginator.rb
+++ lib/simple_paginator.rb
@@ -1,5 +1,27 @@
 require "simple_paginator/version"
+require 'active_support/core_ext/class'
+require 'active_support/concern'
+require 'active_record'
 
 module SimplePaginator
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  DEFAULT_PAR_PAGE = 20
+  DEFAULT_MAX_PAGE = 10
+
+  included do
+    cattr_accessor :par_page, :max_page
+    self.par_page = DEFAULT_PAR_PAGE
+    self.max_page = DEFAULT_MAX_PAGE
+    scope :paged, ->(page) {
+      page_number = [page.to_i, 1].max # 最低でも 1
+      if page_number > max_page
+        none
+      elsif page_number == max_page
+        limit(par_page).offset((page_number-1)*par_page)
+      else
+        limit(par_page+1).offset((page_number-1)*par_page)
+      end
+    }
+  end
 end
--- spec/simple_paginator_spec.rb
+++ spec/simple_paginator_spec.rb
@@ -1,9 +1,13 @@
 require_relative 'spec_helper'
 
+class Post < ActiveRecord::Base
+  include SimplePaginator
+end
+
 describe SimplePaginator do
-  describe 'defined?' do
-    it 'SimplePaginatorが定義されている' do
-      expect(defined?(SimplePaginator)).to eq('constant')
-    end
+  describe 'methods' do
+    specify { expect(Post).to be_respond_to(:paged) }
+    specify { expect(Post).to be_respond_to(:par_page) }
+    specify { expect(Post).to be_respond_to(:max_page) }
   end
 end
